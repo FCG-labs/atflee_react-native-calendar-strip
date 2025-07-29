c28aaba18cf2c5cf3e4dc7d81e4261f33833cc4e
FCG <143424305+FCG-labs@users.noreply.github.com>
2025-07-29 19:16:31 +0900

    -

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 const CalendarStrip = require('./src/components/CalendarStrip');
-const CalendarController = require('./src/controllers/CalendarController');
-const WeekSelector = require('./src/WeekSelector');
-const Scroller = require('./src/Scroller');
 
 module.exports = CalendarStrip;
 module.exports.CalendarStrip = CalendarStrip;
-module.exports.CalendarController = CalendarController;
-module.exports.WeekSelector = WeekSelector;
-module.exports.Scroller = Scroller;
